78526b6f45f383f3b2a7749b7ff8262e06506cde
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-25 14:11:13 +0300

    feat: Add UnauthorizedException for handling unauthorized access attempts

--- src/main/java/com/fsweb/twitterapi/exception/UnauthorizedException.java
+++ src/main/java/com/fsweb/twitterapi/exception/UnauthorizedException.java
@@ -1,7 +1,16 @@
 package com.fsweb.twitterapi.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED) // Bu istisna fırlatıldığında HTTP 401 Unauthorized döneceğini belirtir.
 public class UnauthorizedException extends RuntimeException {
-  public UnauthorizedException(String message) {
-    super(message);
-  }
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+    public UnauthorizedException() {
+        super("You are not authorized to perform this action.");
+    }
 }
