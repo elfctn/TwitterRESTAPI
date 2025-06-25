0eb50088c90601f41ad84d26bc022173955182b9
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-25 14:12:00 +0300

    feat: Add CustomValidationException for business logic and validation errors

--- src/main/java/com/fsweb/twitterapi/exception/CustomValidationException.java
+++ src/main/java/com/fsweb/twitterapi/exception/CustomValidationException.java
@@ -1,7 +1,21 @@
 package com.fsweb.twitterapi.exception;
 
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//Hem genel validasyon hataları
+// hem de iş kuralı tabanlı (örn: kullanıcı adı zaten var) validasyon hataları için..
+
+
+@ResponseStatus(HttpStatus.BAD_REQUEST) // Bu  fırlatıldığında HTTP 400 Bad Request döneceğini belirtir.
+public class CustomValidationException extends RuntimeException {
+
+    public CustomValidationException(String message) {
         super(message);
     }
+
+    // İstersem daha spesifik alan ve değer ile de constructor ekleyebilirim:
+    // public CustomValidationException(String fieldName, String fieldValue, String message) {
+    //     super(String.format("%s: %s - %s", fieldName, fieldValue, message));
+    // }
 }
