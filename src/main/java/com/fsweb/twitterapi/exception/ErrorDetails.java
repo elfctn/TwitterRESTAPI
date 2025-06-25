c065581031b215202bffafe00de7f43bd1b2abe5
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-25 14:12:29 +0300

    feat: Add ErrorDetails DTO for standardized API error responses

--- src/main/java/com/fsweb/twitterapi/exception/ErrorDetails.java
+++ src/main/java/com/fsweb/twitterapi/exception/ErrorDetails.java
@@ -1,4 +1,20 @@
-package com.fsweb.twitterapi.exception;
+package com.fsweb.twitterapi.exception; // Exception paketi altında olacak
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+//GlobalExceptionHandler'ın döneceği
+// standart hata yanıt formatı için bu DTO'ya ihtiyacım var.
+//Bu aslında bir DTO olmuş olur
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
 }
